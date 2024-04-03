sm8450-mainline/pmaports

commit 7270f472e37a96e545dba0ea4dac055c9c2ac998

treewide: fix tabs, final newline, trailing whitespace (MR 3530)

device/community/device-nokia-n900/APKBUILD
@@ -3,7 +3,7 @@
 # Co-Maintainer: Danct12 <danct12@disroot.org>
 pkgname=device-nokia-n900
 pkgver=8.2
-pkgrel=0
+pkgrel=1
 pkgdesc="Nokia N900"
 url="https://postmarketos.org"
 arch="armv7"
@@ -162,34 +162,34 @@ nonfree_firmware() {
 }
 
 sha512sums="
-a4e9196f8ca31fea6f74af078c1e1df1e7be89b63b5d0d292d7f198cfae5f30247dd002536f0f9251ed96518a54aa2b4783870046283e6cd30093115f60cce8e  deviceinfo
-3ae550b50f963695be30eb0e03c31fe7a92aad73174873efe5b73d446b74da5763b6fe9495b91bbdbe307723f6510ebc6639437904815e9251a094ce9bec5b11  uboot-script.cmd
-3d55e34b95791636e44a5f41754f3d0de039dbba41f7a556d43a95c9e64afcfa930046b4b96b40020b6f196096ffba93514682927e32fa4488686fdd19c6da5a  backlight-enable.sh
-c6012aef28b096141b924fced226ec99fb93eb53b69c064a8887d8fecdf8dc08a3ba3db399e18d88374c6ef4c59013a30699c7e4d76e5cb771040582573a0527  90-touchscreen-dev.rules
-9e72035c88632d12895534e5aa5746f2c130c7dcdafa8702748d62b71ed0e1b5911b6e1f07f9b9e39b6072bbb662f66aead585baa1dcacb7d8c8953ca89b6762  10-nokia-modem.rules
-cd5ff582e935c4d28ba359944b197b5486645451790f59e96d9faa4136bf9010c425bfd3c8e46d4b490b4f36fd3336c1d49dd18bc0e2c5738b81520f356adf20  acpi_handler.sh
-b3612348af1465c6dc21ce30153fc7e2cce3ae7f9bf9ff34d5a560e173e174275c1701dd7c45337fa56caaadb252b9494bddf997a698d665d3a8c01cde6955c7  acpi.map
-e440ed7a3070c17e003b86b72dbe6d8194d01b577ca8dd56dd066f216b6dda32bb965c780950f1789a66f7c948290016b048da9f1cf63aba9e11d7e7fd6873ba  rx51_us.map
-dc585e11bf4e06e36c5c62bcc024eaacecc30437d9da5257df14be05e247a2f2bc208874be3058edc6f87cc2877da2ecafd2f627d9b465d4fd24475fc21fdc71  rx51_ch.map
-082a5166e38296b097e873b0b4aeaf007e594d3bf4470c74e91ee3efedcf28ad25cd55c23dd63e460339898ae08e77e111b0e1092fa5e661db90bb40732103a1  rx51_it.map
-0a3e58a3a81c463937caf508a76461b4cc43f593f0817a52b6581fdd132cc894c0960fe7b950b6e6bfac1fad15cd9dd230c103fcf08a30b44ef7d8fbe31cea28  rx51_fise.map
-181187db6d88b872233f594759373f32fd08065ee340b60f0c3ff06396d99f4b1250192d70a054fcc9e51e067f6cc063c62b7d8dfff3427b292f1d0c766db206  40-xkb.conf
-143c21f0b18a016d37cb44178e9daea09f128a90769b48353c03c3f245cb9b1f7e773b9ccee084973fc78ddd7a18c2642e54888a85bda7c7daecddc9a8c62eff  pointercal
-67acc17a33bc75113300393b4a5bba15319014032407e774d079abf310368e75f4f697dbb4856ba2d2d8ea184b056bb33561f42918de3ee91ab6a77df89a133c  asound.state.speakers
-5b87071834313a389f83d29f86f6e3ea108d0921f17dc918be7a46de7e74c73a4418044ef600a0fbf72c8fad22b7730dcd5bfe3d6d01d76bf01556f8540f0b32  asound.state.headset
-2ae4771eda9b151de3b47dc4e3081eb86b92fc04c4be4b4fbb98c28016762d24008e935bbba8f95ea0732019f615ee43f5d93dacb21bdc3471e505a07a684e26  battery-bq27200
-82038d38f94cb975a8d38914afca49b64957446bef7490ab684efa1df47ede2ea1c769045789bb9fded673345eea01911fbbf85fdb54c28685cad8022bfaafac  ofono
-d9ef88c714e9fce8822f63b7a9d7fc3e1ed472c8c876b44ba524d44efea322839f13ddd2fa652420608427ecf7279bfaac302c9b67667f32796ca21da332164d  calendar
-9791ea5f3f0d1ad4f74e2bc18af2ceccb4b8df6262f1566c67f1fd292928bfc75504d0deed32d24495a0d58ca52a52a97ae5ecd2efc34688631af04b131867ad  wifi
-16c31bf28b9dcc256821b153de199bd27decc76e4bde63130c1d1556a63cf2cafb64c4d4df42e00690564fce801c5216e8e97e4262a546440fd4bc2e14eaae1c  conky.conf
-7e8a92b828a3cb823b8df0c67ff3cb72d220f2fdc4557a613ad051baaf7e6e3d0865cdfb5eadff7f4b04302ac650bc485863e26f4d54485d5a1c2389ac0793e3  i3wm.conf
-826a3790b49324c1e61c75b6c0ffc043a2a1d7c13a8c554fb5eae4977af47a1ca93d70ef8c783d712b953b70e18ae58fa4c6a49bf97263398a01a0c4f91ebc8f  i3blocks.conf
-0b80af9fd1f36e6bc06bdfdf48352897234ac7457210649016665da8570a5a64b8a0841b4fbeb64fd7054a5246a64718cf4412f8a53024ce39b28a80984972d8  protip_shell.sh
-a91f98daa60efa2beb2ff6b405097f92edca5f1bbb9e7675499139be52ca2570712f8f06f9032ef29f636f99c8f8da8b992f746eab6424aac04260c16158bcc2  xorg.conf
-38ecdf876b5aa9df2e5ea61c7ebb6f9ca871bdf433029808d028d563dfd740232b3dc21dba098a7622453212af1272e2e782be861b4a96a49fd475a618442d45  10-noroot-suspend
 6b475c12eca8563be48c562a1cb4ad8ca6a05d70fc54b21288b2dd7435a80a6f53351aa870db96d5bbecf1b1c5cb00dfe35f5066e6d3fd0ad0369e66aeef5a21  10-initfs-keymap.files
 4656d3b3ced0a86e2d6315c89322899f646a689e9cb64609aa8ad5a676b23d93706d8a37f3be6f6b12c6b1e8501b7d26ecafecf27322333850513a6805c61910  10-initfs-keymap.sh
+38ecdf876b5aa9df2e5ea61c7ebb6f9ca871bdf433029808d028d563dfd740232b3dc21dba098a7622453212af1272e2e782be861b4a96a49fd475a618442d45  10-noroot-suspend
+b3612348af1465c6dc21ce30153fc7e2cce3ae7f9bf9ff34d5a560e173e174275c1701dd7c45337fa56caaadb252b9494bddf997a698d665d3a8c01cde6955c7  acpi.map
+ac2f8a50280f7d5f8c5ce3ab50d00f1a4c8c1415890347542c684b6e24874a23d57a8f48c62451c46466996decc58276d737bdb892f8b284881d3760948b09bb  acpi_handler.sh
+5b87071834313a389f83d29f86f6e3ea108d0921f17dc918be7a46de7e74c73a4418044ef600a0fbf72c8fad22b7730dcd5bfe3d6d01d76bf01556f8540f0b32  asound.state.headset
+67acc17a33bc75113300393b4a5bba15319014032407e774d079abf310368e75f4f697dbb4856ba2d2d8ea184b056bb33561f42918de3ee91ab6a77df89a133c  asound.state.speakers
+3d55e34b95791636e44a5f41754f3d0de039dbba41f7a556d43a95c9e64afcfa930046b4b96b40020b6f196096ffba93514682927e32fa4488686fdd19c6da5a  backlight-enable.sh
 e6bbac8de1a198ba716f44ade76606e1e0adcc574156b855ac41be5eb5308389d3277fd89cc34b119fbc33d9801ab9f79a6ff213392596bff40bc17e931c05ef  device-nokia-n900.start
-654f2e1b8b7b64fc8c82a5f3c2b76b9d88f56d8dda520c1ce162648d65031bf62702e3bd43e135ab040b927c403f9970d6efae53f957bf1e55954125b358b313  x11-keymap
+a4e9196f8ca31fea6f74af078c1e1df1e7be89b63b5d0d292d7f198cfae5f30247dd002536f0f9251ed96518a54aa2b4783870046283e6cd30093115f60cce8e  deviceinfo
+16c31bf28b9dcc256821b153de199bd27decc76e4bde63130c1d1556a63cf2cafb64c4d4df42e00690564fce801c5216e8e97e4262a546440fd4bc2e14eaae1c  conky.conf
+826a3790b49324c1e61c75b6c0ffc043a2a1d7c13a8c554fb5eae4977af47a1ca93d70ef8c783d712b953b70e18ae58fa4c6a49bf97263398a01a0c4f91ebc8f  i3blocks.conf
+7e8a92b828a3cb823b8df0c67ff3cb72d220f2fdc4557a613ad051baaf7e6e3d0865cdfb5eadff7f4b04302ac650bc485863e26f4d54485d5a1c2389ac0793e3  i3wm.conf
+0b80af9fd1f36e6bc06bdfdf48352897234ac7457210649016665da8570a5a64b8a0841b4fbeb64fd7054a5246a64718cf4412f8a53024ce39b28a80984972d8  protip_shell.sh
+2ae4771eda9b151de3b47dc4e3081eb86b92fc04c4be4b4fbb98c28016762d24008e935bbba8f95ea0732019f615ee43f5d93dacb21bdc3471e505a07a684e26  battery-bq27200
+d9ef88c714e9fce8822f63b7a9d7fc3e1ed472c8c876b44ba524d44efea322839f13ddd2fa652420608427ecf7279bfaac302c9b67667f32796ca21da332164d  calendar
+82038d38f94cb975a8d38914afca49b64957446bef7490ab684efa1df47ede2ea1c769045789bb9fded673345eea01911fbbf85fdb54c28685cad8022bfaafac  ofono
+9791ea5f3f0d1ad4f74e2bc18af2ceccb4b8df6262f1566c67f1fd292928bfc75504d0deed32d24495a0d58ca52a52a97ae5ecd2efc34688631af04b131867ad  wifi
+181187db6d88b872233f594759373f32fd08065ee340b60f0c3ff06396d99f4b1250192d70a054fcc9e51e067f6cc063c62b7d8dfff3427b292f1d0c766db206  40-xkb.conf
+dc585e11bf4e06e36c5c62bcc024eaacecc30437d9da5257df14be05e247a2f2bc208874be3058edc6f87cc2877da2ecafd2f627d9b465d4fd24475fc21fdc71  rx51_ch.map
+0a3e58a3a81c463937caf508a76461b4cc43f593f0817a52b6581fdd132cc894c0960fe7b950b6e6bfac1fad15cd9dd230c103fcf08a30b44ef7d8fbe31cea28  rx51_fise.map
+082a5166e38296b097e873b0b4aeaf007e594d3bf4470c74e91ee3efedcf28ad25cd55c23dd63e460339898ae08e77e111b0e1092fa5e661db90bb40732103a1  rx51_it.map
+e440ed7a3070c17e003b86b72dbe6d8194d01b577ca8dd56dd066f216b6dda32bb965c780950f1789a66f7c948290016b048da9f1cf63aba9e11d7e7fd6873ba  rx51_us.map
 b50e7f37ffb1fa0bb782b117a4e8bcfdb722b6127c3e5d643e90cd901a1f4e65e77a0773ed418673c8195595347482be0c8096f5a723e840315ad61ccd043b66  n900-wlan-data.initd
+143c21f0b18a016d37cb44178e9daea09f128a90769b48353c03c3f245cb9b1f7e773b9ccee084973fc78ddd7a18c2642e54888a85bda7c7daecddc9a8c62eff  pointercal
+3ae550b50f963695be30eb0e03c31fe7a92aad73174873efe5b73d446b74da5763b6fe9495b91bbdbe307723f6510ebc6639437904815e9251a094ce9bec5b11  uboot-script.cmd
+9e72035c88632d12895534e5aa5746f2c130c7dcdafa8702748d62b71ed0e1b5911b6e1f07f9b9e39b6072bbb662f66aead585baa1dcacb7d8c8953ca89b6762  10-nokia-modem.rules
+c6012aef28b096141b924fced226ec99fb93eb53b69c064a8887d8fecdf8dc08a3ba3db399e18d88374c6ef4c59013a30699c7e4d76e5cb771040582573a0527  90-touchscreen-dev.rules
+654f2e1b8b7b64fc8c82a5f3c2b76b9d88f56d8dda520c1ce162648d65031bf62702e3bd43e135ab040b927c403f9970d6efae53f957bf1e55954125b358b313  x11-keymap
+a91f98daa60efa2beb2ff6b405097f92edca5f1bbb9e7675499139be52ca2570712f8f06f9032ef29f636f99c8f8da8b992f746eab6424aac04260c16158bcc2  xorg.conf
 "

device/community/device-nokia-n900/acpi_handler.sh
@@ -4,13 +4,13 @@ cmd=$( echo $0 | awk '{i=split($0,a,"/"); print a[i]}' )
 
 function adjust_keypad_bl {
 	for i in $(seq 1 6); do
-    		echo $1 > /sys/class/leds/lp5523\:kb$i/brightness
+			echo $1 > /sys/class/leds/lp5523\:kb$i/brightness
 	done
 }
 
 case $cmd in
 	KP_SLIDE_OPEN)
-		adjust_keypad_bl 255	
+		adjust_keypad_bl 255
 		;;
 	KP_SLIDE_CLOSE)
 		adjust_keypad_bl 0
@@ -51,36 +51,36 @@ case $cmd in
 	SCRNLCK_UP)
 		echo "Not implemented yet"
 		;;
-        HEADPHONE_INSERT)
-                alsactl restore -f /var/lib/alsa/asound.state.headset
-                ;;
-        HEADPHONE_REMOVE)
-                alsactl restore -f /var/lib/alsa/asound.state.speakers
-                ;;
-        MICROPHONE_INSERT)
-                echo "Not implemented yet"
-                ;;
-        MICROPHONE_REMOVE)
-                echo "Not implemented yet"
-                ;;
-        PWR_BTN_DWN)
-                echo "Not implemented yet"
-                ;;
-        PWR_BTN_UP)
-                echo "Not implemented yet"
-                ;;
-        VIDEOOUT_INSERT)
-                echo "Not implemented yet"
-                ;;
-        VIDEOOUT_REMOVE)
-                echo "Not implemented yet"
-                ;;
-        VOL_DWN)
-                echo "Not implemented yet"
-                ;;
-        VOL_UP)
-                echo "Not implemented yet"
-                ;;
+		HEADPHONE_INSERT)
+				alsactl restore -f /var/lib/alsa/asound.state.headset
+				;;
+		HEADPHONE_REMOVE)
+				alsactl restore -f /var/lib/alsa/asound.state.speakers
+				;;
+		MICROPHONE_INSERT)
+				echo "Not implemented yet"
+				;;
+		MICROPHONE_REMOVE)
+				echo "Not implemented yet"
+				;;
+		PWR_BTN_DWN)
+				echo "Not implemented yet"
+				;;
+		PWR_BTN_UP)
+				echo "Not implemented yet"
+				;;
+		VIDEOOUT_INSERT)
+				echo "Not implemented yet"
+				;;
+		VIDEOOUT_REMOVE)
+				echo "Not implemented yet"
+				;;
+		VOL_DWN)
+				echo "Not implemented yet"
+				;;
+		VOL_UP)
+				echo "Not implemented yet"
+				;;
 	*)
 		echo "Unknown event"
 		exit 1

cross/grub-x86/APKBUILD
@@ -1,6 +1,6 @@
 pkgname=grub-x86
 pkgver=1
-pkgrel=1
+pkgrel=2
 pkgdesc="GRUB i386-efi image"
 url="https://www.gnu.org/software/grub/"
 license="GPL-3.0-or-later"
@@ -10,13 +10,13 @@ options="pmb:strict"
 source="grub_early.cfg"
 
 package() {
-    mkdir -p "$pkgdir/boot/EFI/Boot"
-    grub_mod="all_video disk part_gpt part_msdos linux normal configfile search search_label efi_gop fat iso9660 cat echo ls test true help gzio"
-    grub-mkimage --config="$srcdir/grub_early.cfg" \
-        --prefix="" \
-        --output="$pkgdir/boot/EFI/Boot/bootia32.efi" \
-        --format="i386-efi" --compression="xz" \
-        $grub_mod
+	mkdir -p "$pkgdir/boot/EFI/Boot"
+	grub_mod="all_video disk part_gpt part_msdos linux normal configfile search search_label efi_gop fat iso9660 cat echo ls test true help gzio"
+	grub-mkimage --config="$srcdir/grub_early.cfg" \
+		--prefix="" \
+		--output="$pkgdir/boot/EFI/Boot/bootia32.efi" \
+		--format="i386-efi" --compression="xz" \
+		$grub_mod
 }
 
 sha512sums="0f9357d4eea089628fbceb1e3e96d21a3f4329aeb6743158a6f877f967ef274506404eba949d5f01b7e1adea149f9a3aef992a5e8cd4181f93ab00909b1c5ecd  grub_early.cfg"

device/community/device-odroid-hc2/APKBUILD
@@ -4,7 +4,7 @@
 pkgname=device-odroid-hc2
 pkgdesc="ODROID HC2"
 pkgver=0.7
-pkgrel=1
+pkgrel=2
 url="https://postmarketos.org"
 license="MIT"
 arch="armv7"
@@ -56,5 +56,5 @@ tools() {
 sha512sums="
 69b4fa44985adc3b2de5a516fa54448024973633d53130eee6d5055b1ccf9aa4b83fe0cf3ee30c69c5defcbe20e776995b8b4682e0503c76e0189a63cd582950  deviceinfo
 f3e7a39d53c072f4ae68fcca241f88b05939052ff1c9bd8d45b47d2b336f3c239689857051ff3c7a96caefd5c10d2e34d9b921c62f993f80faca2999eb7beab8  uboot-script.cmd
-669a0561251f287f072c9c1fc37d89b49a47fc4a4e5134c9e6681750ae960e5827043166136b7d5b9ad52c931b060bdcb6d0b5335c2e9fdb4eefd82f131ded93  install-rootfs-hdd.sh
+09a2191dce5b5c8fdf80c4473d9773a51f2c633db51477f2f64983dd030d7800fd80999a228ec7efe3f9feafeba35902bff68cf95e6ae59048dc64424f402b63  install-rootfs-hdd.sh
 "

device/main/device-pine64-pinephone/APKBUILD
@@ -5,7 +5,7 @@
 # Co-Maintainer: Dylan Van Assche <me@dylanvanassche.be>
 pkgname=device-pine64-pinephone
 pkgver=0.43
-pkgrel=1
+pkgrel=2
 pkgdesc="PINE64 PinePhone"
 url="https://postmarketos.org"
 license="MIT"
@@ -216,7 +216,7 @@ bac664c504d9a3adc6e3d25f24ac63cf7c449a82374544a5b56c259dc25af580897b9dc992865ac8
 9cb52e3c5d63dde68296f4c1f9b691629182f82181a7f0877a72aad6870698ea9e7d628309970afbcce01041274054d304de1ee3faf8c1d101f7f8e988a43098  90-modem-eg25.rules
 ed0e03f7e1c0348c06be9a7922414469245604ce813bbf0c8910ef211cb993bbb0d0d4615c9c961d50712415f85d6b73e0cf02780ac784666aac56e4889341e4  UPower.conf
 4a3e607a8e8249668195c472ba181149add65be515360b4d0f465b9c798e7bf8b2f2e431eb3e95cd8620c2a580779c665762f0386239b2fb2a64352e46702edb  deviceinfo
-ccbc83b84b5028bc2c8e526759004ce71b50b2675ecffee98f5676c70a3332197a231ff9d2fd46444dd3c0a637ec08ce6125b18240fe6bfc13f624a15192e648  gpsd_device-hook.sh
+348a1c4b82ffb8d2535b9a659f20d1ebd9a34a6eb8ead1016012c680de6cca401e1883319b2c0206063eb316f0dec274877fb66ee28869592ebea1f10b7b11da  gpsd_device-hook.sh
 1017fc3f325227da58c77abdc59e9735288d91a7ccc63ec784fe0241c523786b617a11ce8045dab2a74ca12a7dd70aaa334af91836418db1e96a3266fecaa4fd  gpsd_pinephone.initd
 3dd6d612c381cb0002049d1974d8fb5aa5a53a1eb4d6bcbf62eb2ad52cfdc45f0f6ad24a699716d3513b0371aa1316f25dc72afc10d7176cc3b99d0965c3f030  hwtest.ini
 6bc22b369b2d9058b702ac7a7862abaefaf8179b43491868bdb008d181b59104f1ded89db1da51a810e1ccdc803a579b924a438674283a65afc3a9c4d2894caf  kirigami-lowpower.sh

device/main/device-qemu-amd64/APKBUILD
@@ -3,7 +3,7 @@
 # Co-Maintainer: Daniele Debernardi <drebrez@gmail.com>
 pkgname=device-qemu-amd64
 pkgver=3
-pkgrel=2
+pkgrel=3
 pkgdesc="Simulated device in QEMU (x86_64)"
 url="https://postmarketos.org"
 arch="x86_64"
@@ -73,5 +73,5 @@ kernel_edge() {
 sha512sums="
 0c9b4b50073dc6bf6bfecb07370254f0dd8a277acbc1d90a605c7891e7d00c1c211545b296b7a1a7a6e00bce05a4197102fcfa449f113f2b500d8879ad29c6bf  deviceinfo
 99d32eed6c5cda59e91516e982c5bd5165ff718133e2411a0dbba04e2057d1dfad49a75e5cc67140d0e0adcbe1383671bd2892335929b782a5b19f5472e635ad  mce-display-blanking.conf
-47b27c7572b8737988488f7eb23b9e68f9a944e22baafe1c78355d2514a2554cf41d99b29fca12238eb13a6f5d53f00ca89b94e534e8461ebab72256dcf0f142  weston.ini
+de794566118f1744d068a94e6a75b61d43f6749a4b0871a5270fa7a2048164d609c71fcffa61845c2a7dd4cb5fbeb72c0e4f8b73b382f36d6ff0bcc9b8a5ae25  weston.ini
 "

device/testing/device-htc-vivo/APKBUILD
@@ -2,7 +2,7 @@
 pkgname=device-htc-vivo
 pkgdesc="Incredible S"
 pkgver=1
-pkgrel=11
+pkgrel=12
 url="https://postmarketos.org"
 license="MIT"
 arch="armhf"
@@ -20,5 +20,5 @@ package() {
 }
 sha512sums="
 d32ce8a022277443aaf2da3dc834369a3f3669cceb78be46b9d1c841f884f43aa721374353e95a3b99b705f9aedf87336c1529c97407a1ee4b1651a8cc420462  deviceinfo
-2d02a02649ee3dc28041045086ffe11c9077067393954295343efb80727432902494da4c3b68f992880f3c0f01a9d164c2517f51519c92445e3ed88872a71e2f  initfs-hook.sh
+765abaad5fda0dfda5e62e1e48414d3eea822198f39d0792171a04d73f86f6273a6e562afab430cbfcdd084412568a0ad03d012e9c40c5564f5cd1b9c0f181eb  initfs-hook.sh
 "

device/testing/device-lenovo-karate/APKBUILD
@@ -2,7 +2,7 @@
 pkgname=device-lenovo-karate
 pkgdesc="Lenovo K6 Power"
 pkgver=0.1
-pkgrel=16
+pkgrel=17
 url="https://postmarketos.org"
 license="MIT"
 arch="aarch64"
@@ -30,5 +30,5 @@ nonfree_firmware() {
 
 sha512sums="
 b51f3fa7782afc75363675841e1c7160b924082a2446bb1d9dbeb23adc164b98c56c1e496a78e07e7b404b446e801428605b174498d99538d6b4142d99d0de25  deviceinfo
-25086c95c67c27151112efac1dcf7792589c990cc0a93ee910654445a3f9cc66c4a5ab656b3256224b39c49fe104540757cc36abf9de19f47c28c4fa82c4f552  90-android-touch-dev.rules
+c6a76928d81a36d54734a6061f73f41ce0a6108cf151eb0ef19c96ac5c108e05996d9bb8b57055037e8e0cb8ea0fb0e7dc3d1f047e1d974f7ea1375754ed0962  90-android-touch-dev.rules
 "

commit c5bb75e4d0d19d46091bae897e2b13d3d7954c92

device-nokia-n900: acpi_handler: fix indentation (MR 4988)
Fixes: 7270f472e37a ("treewide: fix tabs, final newline, trailing whitespace"

[ci:skip-build]: already built successfully in CI

device/community/device-nokia-n900/APKBUILD
@@ -2,7 +2,7 @@
 # Maintainer: Sicelo <absicsz@gmail.com>
 # Co-Maintainer: Danct12 <danct12@disroot.org>
 pkgname=device-nokia-n900
-pkgver=18
+pkgver=19
 pkgrel=0
 pkgdesc="Nokia N900"
 url="https://postmarketos.org"
@@ -188,7 +188,7 @@ sha512sums="
 6b475c12eca8563be48c562a1cb4ad8ca6a05d70fc54b21288b2dd7435a80a6f53351aa870db96d5bbecf1b1c5cb00dfe35f5066e6d3fd0ad0369e66aeef5a21  10-initfs-keymap.files
 4656d3b3ced0a86e2d6315c89322899f646a689e9cb64609aa8ad5a676b23d93706d8a37f3be6f6b12c6b1e8501b7d26ecafecf27322333850513a6805c61910  10-initfs-keymap.sh
 2e6c324c2a0627fb37ee2feff249d6c828116a9c1a32d572481dddc12648c98b726eb3a475838398742f76a60e3f2249d7707502360473b8bf2e794bf22f57ce  acpi.map
-225f5b7b2e6767f8a2dcd534d17463d7e1022b671dddb887aa7fb84fbf0da24326e631ef4c063043f218d10ac5e3ad4dd13f3472f9a54977d643edc996154fa6  acpi_handler.sh
+76a9aa439e59706a95dee54648b336a9837fb908eafbe21832ef9c3551bc48e5a3f7bdca8c8458bba7e721c204bea6f528060b9180b09e4ffa3b9cc163168540  acpi_handler.sh
 5b87071834313a389f83d29f86f6e3ea108d0921f17dc918be7a46de7e74c73a4418044ef600a0fbf72c8fad22b7730dcd5bfe3d6d01d76bf01556f8540f0b32  asound.state.headset
 67acc17a33bc75113300393b4a5bba15319014032407e774d079abf310368e75f4f697dbb4856ba2d2d8ea184b056bb33561f42918de3ee91ab6a77df89a133c  asound.state.speakers
 3d55e34b95791636e44a5f41754f3d0de039dbba41f7a556d43a95c9e64afcfa930046b4b96b40020b6f196096ffba93514682927e32fa4488686fdd19c6da5a  backlight-enable.sh

device/community/device-nokia-n900/acpi_handler.sh
@@ -42,24 +42,24 @@ case $cmd in
 	SCRNLCK_UP)
 		echo "Not implemented yet"
 		;;
-		HEADPHONE_INSERT)
-				alsactl restore -f /var/lib/alsa/asound.state.headset
-				;;
-		HEADPHONE_REMOVE)
-				alsactl restore -f /var/lib/alsa/asound.state.speakers
-				;;
-		MICROPHONE_INSERT)
-				echo "Not implemented yet"
-				;;
-		MICROPHONE_REMOVE)
-				echo "Not implemented yet"
-				;;
-		VIDEOOUT_INSERT)
-				echo "Not implemented yet"
-				;;
-		VIDEOOUT_REMOVE)
-				echo "Not implemented yet"
-				;;
+	HEADPHONE_INSERT)
+		alsactl restore -f /var/lib/alsa/asound.state.headset
+		;;
+	HEADPHONE_REMOVE)
+		alsactl restore -f /var/lib/alsa/asound.state.speakers
+		;;
+	MICROPHONE_INSERT)
+		echo "Not implemented yet"
+		;;
+	MICROPHONE_REMOVE)
+		echo "Not implemented yet"
+		;;
+	VIDEOOUT_INSERT)
+		echo "Not implemented yet"
+		;;
+	VIDEOOUT_REMOVE)
+		echo "Not implemented yet"
+		;;
 	*)
 		echo "Unknown event"
 		exit 1