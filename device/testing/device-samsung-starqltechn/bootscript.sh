a7d0d3f85b9ed13fbd9d7bec383e5160eb556064
Dzmitry Sankouski <dsankouski@gmail.com>
2024-05-01 22:17:29 +0300

    device/testing/starqltechn: u-boot: fix kernel command line (MR 5090)
    - Enable console to workaround a race condition with
    display initialisation (https://gitlab.freedesktop.org/drm/msm/-/issues/46)
    - Remove unneeded options from kernel command line

--- device/testing/device-samsung-starqltechn/bootscript.sh
+++ device/testing/device-samsung-starqltechn/bootscript.sh
@@ -1,3 +1,5 @@
-setenv bootargs 'console=tty1 loglevel=15 clk_ignore_unused'
+# FIXME: Console enabled to workaround a race condition with display initialisation
+# See https://gitlab.freedesktop.org/drm/msm/-/issues/46
+setenv bootargs 'console=ttyMSM0,115200'
 
 bootm $prevbl_initrd_start_addr
