53a5a7a8368580a1c2a8b0df250027e0ff3de02b
Dzmitry Sankouski <dsankouski@gmail.com>
2024-05-01 22:35:23 +0300

    device/testing/starqltechn: u-boot: fix FIT image (MR 5090)
    - increase memory for bootm command to 0x5000000
    - change initramfs and fdt loading addresses to
    leave more space for kernel decompression and loading

--- device/testing/device-samsung-starqltechn/bootscript.sh
+++ device/testing/device-samsung-starqltechn/bootscript.sh
@@ -1,5 +1,6 @@
 # FIXME: Console enabled to workaround a race condition with display initialisation
 # See https://gitlab.freedesktop.org/drm/msm/-/issues/46
 setenv bootargs 'console=ttyMSM0,115200'
+setenv bootm_size 0x5000000
 
 bootm $prevbl_initrd_start_addr
