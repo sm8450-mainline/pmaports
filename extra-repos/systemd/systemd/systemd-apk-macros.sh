ca7c0be4ad7e3ac6ef9a2615ac7187e6a9028970
Clayton Craft <clayton@craftyguy.net>
2024-07-19 13:13:42 -0700

    systemd/systemd: add macros for package install scripts (MR 5377) (MR 5563)
    These macros are based off of the upstream systemd RPM macros, but it
    doesn't implement all of the macro functionality upstream. Initially
    they will be used in the install scripts for apkbuilds that provide
    systemd service unit files.
    
    For example, a `$pkgname-systemd.post-install` script might have:
        . /usr/lib/systemd/systemd-apk-macros.sh
        systemd_service-post_install system sshd
    
    And a `$pkgname-systemd.pre-deinstall` script might have:
        . /usr/lib/systemd/systemd-apk-macros.sh
        systemd_service_pre_deinstall user pipewire
    
    One day maybe these could be upstreamed to systemd to provide some
    apk/abuild compatible macros.
    
    Co-authored-by: jane400 <pmos@j4ne.de>

---
+++ extra-repos/systemd/systemd/systemd-apk-macros.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+# systemd.preset reference:
+# 	https://www.freedesktop.org/software/systemd/man/latest/systemd.preset.html
+# Systemd macro file for RPM:
+# 	https://github.com/systemd/systemd/blob/main/src/rpm/macros.systemd.in
+# 	https://github.com/systemd/systemd/blob/main/src/rpm/systemd-update-helper.in
+
+SYSTEMD_BUS_TIMEOUT=5s
+
+is_systemd_running() {
+	[ -d "/run/systemd/running" ]
+}
+
+# runs `systemctl preset` on the list of services of the given type
+# $1: type of service, supported values: "user" or "system"
+# $@: list of service unit names, as you'd pass to `systemctl preset`
+# FIXME: this may not work with wildcards in unit names, and other input you
+# can give to the preset command
+systemd_service_post_install() {
+	local type="$1"
+	shift
+
+	if [ -z "$type" ]; then
+		echo "service type (user or service) not given"
+		exit 1
+	fi
+
+	if ! command -v systemctl > /dev/null; then
+		echo "systemctl is not available"
+		exit 1
+	fi
+
+	case "$type" in
+		"user")
+			systemctl --no-reload preset --global "$@"
+			;;
+		"system")
+			systemctl --no-reload preset "$@" || true
+			;;
+		*)
+			echo "unsupported service type: $type"
+		exit 1
+	esac
+}
+
+# disables the list of services of the given type
+# $1: type of service, supported values: "user" or "system"
+# $@: list of service unit names, as you'd pass to `systemctl disable`
+# FIXME: this may not work with wildcards in unit names, and other input you
+# can give to the disable command
+systemd_service_pre_deinstall() {
+	local type="$1"
+	shift
+
+	if [ -z "$type" ]; then
+		echo "service type (user/service) not given"
+		exit 1
+	fi
+
+	if ! command -v systemctl > /dev/null; then
+		echo "systemctl is not available"
+		exit 1
+	fi
+
+	case "$type" in
+		"user")
+			systemctl --global disable --no-warn "$@"
+			if is_systemd_running; then
+				local users
+				users=$(systemctl list-units 'user@*' --legend=no | sed -n -r 's/.*user@([0-9]+).service.*/\1/p')
+				for user in $users; do
+					systemctl --user -M "$user@" disable --now --no-warn "$@" &
+				done
+				wait
+			fi
+			;;
+		"system")
+			if is_systemd_running; then
+				systemctl --no-reload disable --now --no-warn $system_preset
+			else
+				systemctl --no-reload disable --no-warn $system_preset
+			fi
+			;;
+		*)
+			echo "unsupported service type: $type"
+			exit 1
+	esac
+}
