dfdccbb1ae347f97c04abe77aabe69bf22570b28
Barnabás Czémán <trabarni@gmail.com>
2024-10-23 10:25:40 +0200

    soc-qcom-msm8974: initial package (MR 5732)
    Add initial SoC pacakge for common configs of msm8974 devices.

---
+++ device/testing/soc-qcom-msm8974/adreno-a330-quirks.sh
@@ -0,0 +1,7 @@
+# Various GPU workarounds for Adreno a330
+
+# The 'ngl' GTK renderer, which is now used by default, has worse
+# performance and is somewhat more prone to crashes. Use the 'gl'
+# renderer until these issues have been sorted out.
+#
+export GSK_RENDERER=gl
