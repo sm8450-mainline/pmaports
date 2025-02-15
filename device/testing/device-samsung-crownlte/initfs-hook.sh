dcf72bea5cb47fa0127e425606b767a28184e106
r00t <r00t@constancy.org>
2025-02-15 08:00:40 +0100

    samsung-crownlte: workaround for touchscreen not working when booted by plugging charger (MR 6219)
    [ci:skip-build]: already built successfully in CI

--- device/testing/device-samsung-crownlte/initfs-hook.sh
+++ device/testing/device-samsung-crownlte/initfs-hook.sh
@@ -3,3 +3,17 @@
 # blank and unblank for pmOS splash screen
 echo 1 > /sys/class/graphics/fb0/blank
 echo 0 > /sys/class/graphics/fb0/blank
+
+if
+	# phone booted by plugging in charger,
+	# touchscreen will not work if we continue booting
+	grep -q androidboot.mode=charger /proc/cmdline
+then
+	# allow for some charging to avoid infinite bootloop if critically low,
+	# "poor man's charging-sdl"
+	if grep -qx 0 /sys/class/power_supply/battery/capacity ; then sleep 24 ; fi
+
+	# a reboot suffices to get the phone working normally,
+	# this will show the samsung splash, unlike the initial boot
+	reboot -ff
+fi
