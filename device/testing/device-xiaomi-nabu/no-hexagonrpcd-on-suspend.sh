d1143376f8dc9d3832ed926531a75e1ffe93564d
panpanpanpan <panjeoc@proton.me>
2025-03-14 10:11:32 -0500

    device-xiaomi-nabu: Remove subpackages and hexagonrpcd to simplify the package (MR 6317)
    As recommended on matrix, there is no need to keep everything in subpackages,
    so they are being removed. Also, hexagonrpcd is being removed as it
    doesn't work on this device’s soc yet.
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6317
    
    [ci:skip-build]: already built successfully in CI

--- device/testing/device-xiaomi-nabu/no-hexagonrpcd-on-suspend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = "pre" ]
-then
-	# If HexagonRPCD listens for a remote method call while the device is
-	# waking up, the DSP may crash. Assume that HexagonRPCD has fulfilled
-	# all incoming remote method invocations, and stop it.
-	rc-service hexagonrpcd-sdsp stop
-fi
