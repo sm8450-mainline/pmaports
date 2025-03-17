9b19e3e2486df4dea912888d83a7f9f478604a26
Sicelo A. Mhlongo <absicsz@gmail.com>
2025-03-17 21:27:54 +0200

    main/postmarketos-base: drop udev-based firmware loading
    udev no longer supports using userspace to service kernel requests for
    firmware. While the kernel can be configured to send such requests to
    userspace, udev will not do anything about them.
    
    Reference: https://github.com/eudev-project/eudev/commit/3b717594600fa717cdf9bcfd0c7c1b703b245482
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6332
    [ci:skip-build]: already built successfully in CI

--- .ci/shellcheck.sh
+++ .ci/shellcheck.sh
@@ -15,7 +15,6 @@ fi
 # Shell: shellcheck
 sh_files="
 	./main/mdss-fb-init-hack/mdss-fb-init-hack.sh
-	./main/postmarketos-base/rootfs-usr-lib-firmwareload.sh
 	./main/postmarketos-base-ui/rootfs-usr-lib-NetworkManager-dispatcher.d-50-dns-filter.sh
 	./main/postmarketos-base-ui/rootfs-usr-lib-NetworkManager-dispatcher.d-50-tethering.sh
 	./main/postmarketos-installkernel/installkernel-pmos
