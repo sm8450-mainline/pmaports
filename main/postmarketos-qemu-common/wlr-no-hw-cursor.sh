97b9d77632b41d05a77c77120096dd517e66236e
Clayton Craft <clayton@craftyguy.net>
2025-05-13 21:38:43 -0700

    main/postmarketos-qemu-common: disable hw cursors on wlroots
    This partially works around #2246
    
    This is setting a var specific to wlroots, BUT many UIs use wlroots and some
    (phosh) vendor it so adding a single text file with 1 line in it seems better
    than having at least 2 extra subpackages (1 for installing if phoc, 1 for
    installing if wlroots, and others?)
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6508
    Part-of: https://gitlab.postmarketos.org/postmarketos/pmaports/-/merge_requests/6508

---
+++ main/postmarketos-qemu-common/wlr-no-hw-cursor.sh
@@ -0,0 +1,5 @@
+# Disabling hw cursors in wlroots helps with the cursor showing up in the
+# wrong location.
+# https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/issues/2246
+# https://github.com/swaywm/sway/issues/6581
+export WLR_NO_HARDWARE_CURSORS=1
