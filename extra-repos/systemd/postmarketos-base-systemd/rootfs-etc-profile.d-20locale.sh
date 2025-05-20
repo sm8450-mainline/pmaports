3b0f9a1b82469844e9712afd4ec041df617309be
Clayton Craft <clayton@craftyguy.net>
2025-05-20 00:33:16 -0700

    systemd/postmarketos-base-systemd: fix issue with locale.conf being ignored
    Shells (and DEs!) that set environment from /etc/profile.d will end
    up sourcing 20locale.sh from alpine-baselayout. This file will set
    `LANG=C.UTF8`, and this will override whatever systemd-localed/systemd
    sets LANG to. In other words, locale.conf is effectively ignored. I
    hit this issue when debugging why the date in COSMIC was always `M05`
    instead of `May`, and why various COSMIC components failed to detect the
    locale (because it was set to C.UTF8)
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6535
    [ci:skip-build]: already built successfully in CI

---
+++ extra-repos/systemd/postmarketos-base-systemd/rootfs-etc-profile.d-20locale.sh
@@ -0,0 +1,5 @@
+# Important: alpine-baselayout sets LANG here, which can override systemd-localed,
+# so this file has been stubbed out
+#
+# Use `localectl set-locale` instead.
+# See `man 8 systemd-localed` and `man 5 locale.conf`
