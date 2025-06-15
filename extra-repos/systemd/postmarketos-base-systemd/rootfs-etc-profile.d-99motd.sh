c8b5872b5696ed07c5ed45f1a75a70a18a666523
Oliver Smith <ollieparanoid@postmarketos.org>
2025-06-15 17:56:47 +0200

    systemd/postmarketos-base-systemd: remove motd
    Remove the systemd-specific MOTD that says it is still in preview.
    v25.06 is about to come out, the first stable release with systemd!
    
    Any future adjustments to make it use fancy ascii colors etc. should be
    made in postmarketos-base instead.
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6669
    [ci:skip-build]: already built successfully in CI

--- extra-repos/systemd/postmarketos-base-systemd/rootfs-etc-profile.d-99motd.sh
+++
@@ -1,6 +0,0 @@
-# Very minimal ANSI motd
-. /etc/os-release
-
-echo
-echo -e "  Welcome to \033[32mpostmarketOS $VERSION\033[0m/\033[97msystemd\033[0m! ^^\n"
-echo -e "$(cat /etc/motd.ansi | sed 's/^/  /')"
