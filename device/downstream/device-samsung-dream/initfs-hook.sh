94fd3a01db3e0ebfc2c4cb8b416739e20f35f93d
Henrik Grimler <grimler@postmarketos.org>
2025-06-23 17:29:15 +0200

    device-samsung-dream: use ncm instead of rndis in initfs network script
    PostmarketOS now uses ncm instead of rndis, so update script accordingly.
    
    Unfortunately usb networking does not work even after this change when
    I test on dream2lte, so this needs further debugging.
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6706
    [ci:skip-build]: already built successfully in CI

--- device/downstream/device-samsung-dream/initfs-hook.sh
+++ device/downstream/device-samsung-dream/initfs-hook.sh
@@ -9,14 +9,14 @@ CONFIGFS=/config/usb_gadget
 # Create an usb gadet configuration
 mkdir $CONFIGFS/g1 || echo "initfs-hook: Couldn't create $CONFIGFS/g1"
 
-# Create rndis function.
-mkdir $CONFIGFS/g1/functions/rndis.usb0 \
-	|| echo "initfs-hook: Couldn't create $CONFIGFS/g1/functions/rndis.usb0"
+# Create network function.
+mkdir $CONFIGFS/g1/functions/ncm.usb0 \
+	|| echo "initfs-hook: Couldn't create $CONFIGFS/g1/functions/ncm.usb0"
 
 # Create configuration instance for the gadget
 mkdir $CONFIGFS/g1/configs/c.1 \
 	|| echo "initfs-hook: Couldn't create $CONFIGFS/g1/configs/c.1"
 
-# Link the rndis instance to the configuration
-ln -s $CONFIGFS/g1/functions/rndis.usb0 $CONFIGFS/g1/configs/c.1 \
-	|| echo "initfs-hook: Couldn't symlink rndis.usb0"
+# Link the network instance to the configuration
+ln -s $CONFIGFS/g1/functions/ncm.usb0 $CONFIGFS/g1/configs/c.1 \
+	|| echo "initfs-hook: Couldn't symlink ncm.usb0"
