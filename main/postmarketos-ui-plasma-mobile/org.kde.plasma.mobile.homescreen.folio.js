9962707c1eb5434d0febe36d7cf9de17c2323d26
Devin Lin <espidev@gmail.com>
2025-07-15 20:38:06 -0400

    postmarketos-ui-plasma-mobile: add default homescreen
    The current script doesn't do anything because the path was changed
    during the Plasma 5 -> 6 transition. Change the script to supply a
    default homescreen layout.
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/6786
    [ci:skip-build]: already built successfully in CI

---
+++ main/postmarketos-ui-plasma-mobile/org.kde.plasma.mobile.homescreen.folio.js
@@ -0,0 +1,3 @@
+applet.writeConfig('Favourites', '[{"storageId":"org.kde.angelfish.desktop","type":"application"},{"storageId":"org.kde.plasma.dialer.desktop","type":"application"},{"storageId":"org.kde.spacebar.desktop","type":"application"}]')
+applet.writeConfig('Pages', '[[]]')
+applet.reloadConfig()
